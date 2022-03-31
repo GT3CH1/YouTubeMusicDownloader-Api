0d5ceb5f0cec1bd8106ff6902180688052ac79ec
Gavin <gavinpease@gmail.com>
2022-03-31 15:08:24 -0600

    Implement downloading + tagging songs

--- YouTubeDownloader/Controllers/SongController.cs
+++ YouTubeDownloader/Controllers/SongController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using NYoutubeDL;
+using NYoutubeDL.Helpers;
 using YouTubeDownloader.Contexts;
 using YouTubeDownloader.Models;
 
@@ -8,7 +10,7 @@ namespace YouTubeDownloader.Controllers;
 public class SongController : Controller
 {
     private SongsDbContext dbContext;
-    
+
     public SongController(SongsDbContext dbContext)
     {
         this.dbContext = dbContext;
@@ -19,6 +21,7 @@ public class SongController : Controller
     {
         return "string";
     }
+
     // Add a Song to the Database
     [HttpPost]
     [Route("Add")]
@@ -34,7 +37,7 @@ public class SongController : Controller
             return "Song added";
         }
     }
-    
+
     // Add a list of songs to the database
     [HttpPost]
     [Route("AddList")]
@@ -45,7 +48,7 @@ public class SongController : Controller
         dbContext.SaveChanges();
         return "Songs Added";
     }
-    
+
     // Get a list of all songs in the database
     [HttpGet]
     [Route("GetAll")]
@@ -53,5 +56,44 @@ public class SongController : Controller
     {
         return Json(dbContext.Songs.ToList());
     }
-    
+
+    // Download a song from the database
+    [HttpGet]
+    [Route("DownloadAll")]
+    public void DownloadAll()
+    {
+        var list = dbContext.Songs.ToList();
+        foreach (var song in list)
+        {
+            DownloadSong(song);
+        }
+    }
+
+    private void DownloadSong(Song song)
+    {
+        var youtubeDl = new YoutubeDLP();
+        // Set youtubedl path to /usr/bin/youtube-dl
+        youtubeDl.YoutubeDlPath = @"C:\Users\gcpease\.bin\youtube-dl.exe";
+        // Set audio to best
+        youtubeDl.Options.PostProcessingOptions.AudioFormat = Enums.AudioFormat.mp3;
+        // Extract audio
+        youtubeDl.Options.PostProcessingOptions.ExtractAudio = true;
+        // Make Artist/Album directories
+        // Create path to file
+        var dir = $"Songs/{song.Artist}/{song.Album}";
+        var path = $"{dir}/{song.Title}.mp3";
+        Directory.CreateDirectory(dir);
+        // Set output directory
+        youtubeDl.Options.FilesystemOptions.Output = path;
+        // Download the song
+        youtubeDl.VideoUrl = song.Url;
+        if (!System.IO.File.Exists(path))
+            youtubeDl.Download(song.Url);
+        // Tag the song.
+        var file = TagLib.File.Create(path);
+        file.Tag.Album = song.Album;
+        file.Tag.Title = song.Title;
+        file.Tag.AlbumArtists = new[] { song.Artist };
+        file.Save();
+    }
 }
